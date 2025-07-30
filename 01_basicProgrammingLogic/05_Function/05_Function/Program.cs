2be7ad55e30ebaf72a27c98e8d27bea0d77584b7
corvo ×͜× <cviny072@gmail.com>
2025-07-29 21:31:35 -0300

    feat: example function

---
+++ 01_basicProgrammingLogic/05_Function/05_Function/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace learning_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter three numbers: ");
+
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int result = FindMax(num1, num2, num3);
+            Console.WriteLine($"Max = {result}");
+        }
+
+        static int FindMax(int a, int b, int c)
+        {
+            int max;
+
+            if (a > b && a > c)
+            {
+                max = a;
+            }
+            else if (b > c)
+            {
+                max = b;
+            }
+            else
+            {
+                max = c;
+            }
+
+            return max;
+        }
+    }
+}
